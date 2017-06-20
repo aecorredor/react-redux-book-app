0b57e49f88af553a974d718c8e5113cde7def646
Alejandro Corredor <aecorredor93@gmail.com>
2017-06-20 16:27:03 -0400

    finish wiring up all redux containers

---
+++ src/actions/index.js
@@ -0,0 +1,10 @@
+import { BOOK_SELECTED } from './types';
+
+export function selectBook(book) {
+  // selectBook is an ActionCreater, it needs to return an action,
+  // an object with a type property.
+  return {
+    type: BOOK_SELECTED,
+    payload: book
+  };
+}
---
+++ src/containers/book_detail.js
@@ -0,0 +1,23 @@
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+
+class BookDetail extends Component {
+  render() {
+    return this.props.book ?
+      <div>
+        <h3>Details for:</h3>
+        <div>
+          {this.props.book.title}
+        </div>
+      </div> :
+      <div>Select a book to get started</div>
+  }
+}
+
+function mapStateToProps(state) {
+  return {
+    book: state.activeBook
+  };
+}
+
+export default connect(mapStateToProps)(BookDetail);
